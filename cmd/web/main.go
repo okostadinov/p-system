ca2f1bd959c96010af53bac16cb879f8b3f20c92
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2023-10-25 22:56:20 +0300

    add db setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -16,8 +19,24 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	cfg := mysql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "psystem",
+		AllowNativePasswords: true,
+	}
+
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		infoLog:  infoLog,
@@ -32,4 +51,15 @@ func main() {
 
 	infoLog.Printf("Listening on: http://localhost%s\n", *addr)
 	errorLog.Fatal(srv.ListenAndServe())
+}
+
+func openDB(cfg mysql.Config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module p-system.okostadinov.net
 
 go 1.21.2
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require github.com/go-sql-driver/mysql v1.7.1
